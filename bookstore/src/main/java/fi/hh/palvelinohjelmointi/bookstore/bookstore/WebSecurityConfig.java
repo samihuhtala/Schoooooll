8d962a36b138979799765afc21a7ff3c0cfbf53d
Huhtala Sami <a1700353@myy.haaga-helia.fi>
2018-03-02 11:30:00 +0200

    päivitetty oikeuksienrajaus

--- bookstore/src/main/java/fi/hh/palvelinohjelmointi/bookstore/bookstore/WebSecurityConfig.java
+++ bookstore/src/main/java/fi/hh/palvelinohjelmointi/bookstore/bookstore/WebSecurityConfig.java
@@ -17,6 +17,7 @@ protected void configure(HttpSecurity http) throws Exception {
 	.and()
 	.authorizeRequests()
 	.antMatchers("/", "booklist").permitAll()
+	.antMatchers("/delete{id}").hasRole("ADMIN")
 	.anyRequest().authenticated()
 	.and()
 	.formLogin()

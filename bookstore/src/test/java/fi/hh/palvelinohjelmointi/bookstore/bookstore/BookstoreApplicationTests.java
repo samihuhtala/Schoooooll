b1a1470009e833935a42644b585e08609aceaa3a
Huhtala Sami <a1700353@myy.haaga-helia.fi>
2018-03-09 10:12:58 +0200

    testeja

--- bookstore/src/test/java/fi/hh/palvelinohjelmointi/bookstore/bookstore/BookstoreApplicationTests.java
+++ bookstore/src/test/java/fi/hh/palvelinohjelmointi/bookstore/bookstore/BookstoreApplicationTests.java
@@ -1,16 +1,36 @@
 package fi.hh.palvelinohjelmointi.bookstore.bookstore;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import fi.hh.palvelinohjelmointi.bookstore.bookstore.webcontrol.BookController;
+import fi.hh.palvelinohjelmointi.bookstore.bookstore.webcontrol.UserController;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BookstoreApplicationTests {
 
+	
+	@Autowired
+	private BookController bcontorller;
+	
+	@Autowired
+	private UserController ucontoller;
+	
 	@Test
-	public void contextLoads() {
+	public void contexLoads() throws Exception {
+		assertThat(bcontorller).isNotNull();
+		
+	}
+	
+	@Test
+	public void contextLoads() throws Exception {
+		assertThat(ucontoller).isNotNull();
 	}
 
 }
